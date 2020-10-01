a94bd252b3cc3f309b90abbff908d5b9703fd867
alangregorian <42546068+alangregorian@users.noreply.github.com>
2020-09-30 22:12:22 -0500

    started section 6

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,35 @@
+//var income_array = [];
+//var expenses_array = [];
+
+
+
+var budgetController = (function() {
+
+  
+})();
+
+
+
+
+var UIController = (function() {
+  //some code
+})();
+
+
+
+
+var appController = (function(budgetCntlr, UICntrlr) {
+  
+  document.querySelector('.add__btn').addEventListener('click', function() {
+  // 1. get value of add or subtract
+  console.log('button was clicked!')
+  
+  // 2. get value entered into the description
+  // 3. get value entered into the value
+  
+  // 4. add value and description to income if + and expenses if -.
+});
+  
+})(budgetController, UIController);
+
+
